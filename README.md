befd1dbefb169776f31302e0a24adc5ba4d3cd78
Sávio <saviopf22@gmail.com>
2025-11-03 01:54:26 -0300

    create header

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 - command to create: `npm create vue@latest vue-course`
 - install tailwind: `npm install -D tailwindcss@3.3.5 postcss@latest autoprefixer@latest && npx tailwindcss init -p`
 
-[source video](https://www.youtube.com/watch?v=VeNfHj6MhgA)
+[source video 1:01:52-2:56:44](https://www.youtube.com/watch?v=VeNfHj6MhgA)
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue';
+
+</script>
 
 <template>
-  <div class="h-screen bg-accent text-white flex flex-col justify-center items-center gap-4">
-    <h1 class="font-bold">You did it!</h1>
-    <p>
-      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-      documentation
-    </p>
-  </div>
+  <NavBar />
 </template>
 
 <style scoped></style>
