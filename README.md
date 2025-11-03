e7860097273d75dc4002d2e86d3c965c1bd06f60
Sávio <saviopf22@gmail.com>
2025-11-03 01:42:17 -0300

    git init

---
+++ README.md
@@ -0,0 +1,8 @@
+# vue-course
+
+### steps to create project
+
+- command to create: `npm create vue@latest vue-course`
+- install tailwind: `npm install -D tailwindcss@3.3.5 postcss@latest autoprefixer@latest && npx tailwindcss init -p`
+
+[source video](https://www.youtube.com/watch?v=VeNfHj6MhgA)
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="h-screen bg-accent text-white flex flex-col justify-center items-center gap-4">
+    <h1 class="font-bold">You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
